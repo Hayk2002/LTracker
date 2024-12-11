a36c580d37070e41068867f8850f01650f82d1a2
Hayk Shahinyan <shahinyan142@gmail.com>
2024-12-11 21:23:41 +0400

    FEAT: Update admin UI

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,9 +1,12 @@
-import {collection, getDocs} from "firebase/firestore";
-import {firestore} from "./firebase-config";
-import React, {useEffect, useState} from "react";
+import { Avatar, List } from "antd";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+
+import { firestore } from "./firebase-config";
 
 const Dashboard = () => {
-    const [location, setLocation] = useState(null);
+    const [locations, setLocations] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const getCoordinates = async () => {
         try {
@@ -20,26 +23,40 @@ const Dashboard = () => {
             return await Promise.all(data);
         } catch (error) {
             console.error(error);
+        } finally {
+            setLoading(false);
         }
     }
 
     useEffect(() => {
         (async () => {
             const coordinates = await getCoordinates()
-            const id = coordinates[0]?.id
-            localStorage.setItem('locID', JSON.stringify(id))
-            setLocation(coordinates[0])
+            setLocations(coordinates)
         })()
     }, []);
 
-    return location !== null && (
-        <div>
-            <a
-                target="_blank"
-                rel="noreferrer"
-                href={`https://yandex.com/maps/10262/yerevan/?mode=search&sll=${location?.long}%2C${location?.lat}&text=${location?.lat}%2C${location?.long}&z=21`}>
-                Yandex Map
-            </a>
+    return (
+        <div style={{width: 800, height: '100%', margin: "50px auto", padding: 10, border: "1px solid #ccc"}}>
+            <List
+                loading={loading}
+                dataSource={locations}
+                renderItem={(item) => (
+                    <List.Item key={item.id} style={{borderBottom: "1px solid #ccc"}}>
+                        <List.Item.Meta
+                            avatar={<Avatar/>}
+                            title={item.id}
+                            description={`${item.lat}, ${item.long}`}
+                        />
+                        <a
+                            target="_blank"
+                            rel='noreferrer'
+                            href={`https://yandex.com/maps/10262/yerevan/?mode=search&sll=${item?.long}%2C${item?.lat}&text=${item?.lat}%2C${item?.long}&z=21`}
+                        >
+                            Yandex Map
+                        </a>
+                    </List.Item>
+                )}
+            />
         </div>
     )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,13 @@
-import { collection, addDoc, setDoc, doc } from "firebase/firestore";
-import { firestore, db } from "./firebase-config";
 import { useCallback, useEffect } from "react";
+import { collection, addDoc } from "firebase/firestore";
 
-const locID = localStorage.getItem('locID');
+import { firestore } from "./firebase-config";
 
 const Home = () => {
-
     const createCoordinates = async (position) => {
-        console.log(position.coords.latitude, 'lat', position.coords.longitude, 'long')
         const data = { lat:  position.coords.latitude, long: position.coords.longitude }
         try {
-            if (locID !== null && locID !== undefined) {
-                const docRef = doc(db, 'coordinates', locID);
-                await setDoc(docRef, data, { merge: true })
-            } else {
-                await addDoc(collection(firestore, 'coordinates'), data);
-            }
+            await addDoc(collection(firestore, 'coordinates'), data);
         } catch (error) {
             console.error(error);
         }
